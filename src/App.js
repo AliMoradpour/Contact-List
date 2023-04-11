940b761c338b3bca331bf0cdb98b8b65e7a5f1b0
AliMoradpour <Mr.mrdp@yahoo.com>
2023-04-11 14:49:59 +0430

    use LocalStorage for saving contacts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import AddContact from "./Components/AddContact/AddContact";
 import ContactList from "./Components/ContactList/ContactList";
@@ -15,6 +15,16 @@ function App() {
     const filteredContacts = contacts.filter((c) => c.id !== id);
     setContacts(filteredContacts);
   };
+
+  useEffect(() => {
+    const savedContacts = JSON.parse(localStorage.getItem("contacts"));
+    if (savedContacts) setContacts(savedContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
     <main className="App">
       <h1>Contact List</h1>
